a8b6e657f76dd1a1c1ffd03be362b5699a02b83f
Mrliyang <Mrli_yang@163.com>
2019-09-01 13:57:56 +0800

    commit
    Hello World Study.

---
+++ com/study/simple/HelloWorld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author : Leon
+# @Email  : 635685051@qq.com
+# @Time   : 2019-09-01 12:53
+# @Soft   : PyCharm
+
+
+"""
+Hello World
+"""
+
+
+print('Hello World, ', 'I\'m Leon.\n')
